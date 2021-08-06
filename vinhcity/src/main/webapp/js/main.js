d44e5452dbdd584331295a3b202768f665e6f721
nhan0987 <skynetx002@gmail.com>
2021-08-06 16:06:48 +0700

    Thêm 2 theme Vinh và Phường

---
+++ vinhcity/src/main/webapp/js/main.js
@@ -0,0 +1,123 @@
+$(document).ready(function() {
+	$('.namdan-theme .video-player-default-style .video-playlist select').css('max-width', '');
+});
+
+
+//Sync Date
+AUI().ready(
+	function(A){
+		var currentDateTime = setInterval("getCurrentDateTime()", 1000);
+	}
+);
+
+function getCurrentDateTime(){
+
+	var day_of_week = new Array();
+	day_of_week[0] = "Chủ nhật";
+	day_of_week[1] = "Thứ hai";
+	day_of_week[2] = "Thứ ba";
+	day_of_week[3] = "Thứ tư";
+	day_of_week[4] = "Thứ năm";
+	day_of_week[5] = "‎Thứ sáu";
+	day_of_week[6] = "Thứ bảy";
+	
+	var month_of_year = new Array();
+	month_of_year[0] = "01";
+	month_of_year[1] = "02";
+	month_of_year[2] = "03";
+	month_of_year[3] = "04";
+	month_of_year[4] = "05";
+	month_of_year[5] = "06";
+	month_of_year[6] = "07";
+	month_of_year[7] = "08";
+	month_of_year[8] = "09";
+	month_of_year[9] = "10";
+	month_of_year[10] = "11";
+	month_of_year[11] = "12";
+	
+	var now = new Date();   
+	var currentYear= now.getYear();     
+	var currentMonth = now.getMonth();  
+	var currentDate = now.getDate();   
+	var currentDay = now.getDay();
+	var currentHour = now.getHours();
+	var currentMinute = now.getMinutes();
+	var currentSecond = now.getSeconds();
+	
+	if (currentYear < 1000) {
+		currentYear = 1900  + currentYear;
+	}
+	
+	if (currentDate < 10) {
+		currentDate = "0" + currentDate;
+	}  
+			
+	if (currentHour < 10) {
+		currentHour = "0" + currentHour;
+	} 
+			
+	if (currentMinute < 10) {
+		currentMinute = "0" + currentMinute;
+	} 
+			
+	if (currentSecond < 10) {
+		currentSecond = "0" + currentSecond;
+	} 
+	
+	var currentDateValue = day_of_week[currentDay] + ", " + currentDate + "/" + month_of_year[currentMonth] + "/" + currentYear;
+	var currentDateTimeValue = day_of_week[currentDay] + ", ngày " + currentDate + "/" + month_of_year[currentMonth] + "/" + currentYear;
+	var currentTimeValue = currentHour + ":" + currentMinute + ":" + currentSecond;
+	
+	var currentDateWrapper = document.getElementById("current_date");
+	var currentDateTimeWrapper = document.getElementById("current_datetime");
+	var currentTimeWrapper = document.getElementById("current_time");
+	
+	if(currentDateWrapper){
+		currentDateWrapper.innerHTML = currentDateValue;
+	}
+	
+	if(currentDateTimeWrapper){
+		currentDateTimeWrapper.innerHTML = currentDateTimeValue;
+	}
+	
+	if(currentTimeWrapper){
+		currentTimeWrapper.innerHTML = currentTimeValue;
+	}
+}
+
+
+//Nav actions
+$("<i class='fa fa-caret-down'></i>").insertBefore("#mainNav .child-menu");
+
+$('#mainNav li i').on('click', function() {
+	$(this).toggleClass('fa-caret-down fa-caret-up');
+	$(this).parent().toggleClass('open');
+});
+
+
+//Search bar in Navbar
+// window.onload = function() {
+//     document.getElementById('searchInput').onkeypress = function searchKeyPress(event) {
+//        if (event.keyCode == 13) {
+//            document.getElementById('btnSearch').click();
+//        }
+//    };
+// }
+
+Liferay.provide(window, 'doSearch',function(){
+	var A = AUI();
+	var searchInput = A.one("#searchInput");
+
+if(searchInput) {
+	var keywords = searchInput.val();
+	keywords = keywords.replace(/^\s+|\s+$/,"");
+
+			if(keywords !== "") {
+			var url = '/web/cong-thanh-tra/search/-/search/' + keywords;
+			
+			window.location.href = url;
+			};
+		}
+	}, 
+	['aui-base']
+);
